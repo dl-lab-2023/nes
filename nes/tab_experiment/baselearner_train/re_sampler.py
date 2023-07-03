237a687238b694c14ab838e367069469623e6395
cnmicha <10116544+cnmicha@users.noreply.github.com>
2023-07-03 15:28:16 +0200

    Hyperparameter sampling using configspaces library

--- nes/tab_experiment/baselearner_train/re_sampler.py
+++ nes/tab_experiment/baselearner_train/re_sampler.py
@@ -1,24 +1,14 @@
 from typing import Any, Dict, Tuple
 import numpy as np
+from ConfigSpace import ConfigurationSpace, Configuration
 
 
 def sample_random_genotype_hp() -> Tuple[str, Dict[str, Any]]:
-    """
-    returns random hps every time you call the function
-    :returns: string-id, dictionary with hps
-    """
-    sampled_hps = {
-        'lr': np.random.choice([0.1, 0.01, 0.001]),
-        # https://pytorch.org/docs/stable/optim.html
-        'optimizer': np.random.choice(['Adam', 'SGD', 'RMSprop']),
-        'sgd-momentum': np.random.choice([0.9]),
-        'sgd-weight-decay': np.random.choice([3e-5])
-    }
-
-    id = ""
-    for hp_name, hp_value in sampled_hps.items():
-        id = id + f"{hp_name}-{hp_value}|"
-
-    id = id[:-1]  # Remove last "|" character
-
-    return id, sampled_hps
+    config_space = ConfigurationSpace(
+        name="experiment-configspace",
+        space={
+            "AdamWOptimizer:lr": (0.001, 0.1),  # UniformFloat
+            "CosineAnnealingWarmRestarts:n_restarts": (1, 10),  # UniformInt
+        }
+    )
+    return config_space.sample_configuration(size=1)
--- nes/tab_experiment/baselearner_train/train.py
+++ nes/tab_experiment/baselearner_train/train.py
@@ -6,7 +6,7 @@ import time
 from autoPyTorch.api.tabular_classification import TabularClassificationTask
 from nes.tab_experiment.baselearner_train.re_sampler import sample_random_genotype_hp
 from nes.tab_experiment.baselearner_train.utilities import get_data
-from ConfigSpace.configuration_space import Configuration
+from ConfigSpace import ConfigurationSpace, Configuration
 
 import torch
 
@@ -132,27 +132,9 @@ def train_using_auto_pytorch(tab_task_id, seed, configuration, num_epochs, bslrn
     # TODO store metrics
 
 
-def get_auto_pytorch_configuration(hp_config):
-    # We're following https://automl.github.io/Auto-PyTorch/master/examples/40_advanced/example_single_configuration.html#sphx-glr-examples-40-advanced-example-single-configuration-py
-
-    # Use configspaces lib
-    # TODO fix the import of "Configuration"!
-    # This is the library code used by the requirements.txt, with the used version from 2021:
-    # https://github.com/automl/ConfigSpace/tree/f3b5ece99ff132c56c8d741a933a82d3bdc3f635
-    configuration = Configuration()
-
-    return configuration
-
-
 # TODO replace this function by a cluster script
 if __name__ == "__main__":
-    hp_id, hp_config = sample_random_genotype_hp()
+    configuration = sample_random_genotype_hp()
+    train_using_auto_pytorch(1, 1, configuration, 1, 1, None)
 
-    auto_pytorch_configuration = get_auto_pytorch_configuration(hp_config)
-
-    train_using_auto_pytorch(1, 1, auto_pytorch_configuration, 1, 1, None)
-
-
-# TODO is there a possibility to let auto pytorch do the sampling? see
-# https://automl.github.io/Auto-PyTorch/master/examples/40_advanced/example_custom_configuration_space.html#sphx-glr-examples-40-advanced-example-custom-configuration-space-py
-# this looks good, but we need to check whether it's possible to parallelize this with SLURM
+# We're following https://automl.github.io/Auto-PyTorch/master/examples/40_advanced/example_single_configuration.html#sphx-glr-examples-40-advanced-example-single-configuration-py
