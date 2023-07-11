15ebdcf6444e165999454af9cb5a023746d174b3
cnmicha <10116544+cnmicha@users.noreply.github.com>
2023-07-11 14:10:05 +0200

    Modify launcher script to work with the NEMO cluster (moab job manager)

--- own_pipeline/cluster_launcher.sh
+++ own_pipeline/cluster_launcher.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-#SBATCH -a 0-399
-#SBATCH -o ./cluster_logs/own_pipeline/%A-%a.o
-#SBATCH -e ./cluster_logs/own_pipeline/%A-%a.e
-#SBATCH --gres=gpu:1  # reserves GPUs
-#SBATCH -J own_pipeline # sets the job name. If not specified, the file name will be used as job name
+# See https://wiki.bwhpc.de/e/NEMO/Moab#Job_array_example
 
-# Info
-echo "Workingdir: $PWD"
+set -e
+
+# The NEMO cluster offers getting conda using the "module" feature
+# Unfortunately, the the most recent available Python version was 3.6.
+# Therefore we installed Miniconda manually using https://docs.conda.io/en/latest/miniconda.html#latest-miniconda-installer-links
+
+MOAB_WORKSPACE_NAME=dl-lab
+CONDA_BIN=$(ws_find $MOAB_WORKSPACE_NAME)/conda/bin/conda
+CONDA_WORKSPACE_NAME=dl-lab
+
 echo "Started at $(date)"
-echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION"
+echo "MOAB_JOBARRAYINDEX (this job id): $MOAB_JOBARRAYINDEX"
+echo "MOAB_JOBARRAYRANGE (num total jobs of this task): $MOAB_JOBARRAYRANGE"
 
-# Activate virtual environment
-source .venv/bin/activate
+cd $(ws_find $MOAB_WORKSPACE_NAME)
+echo "PWD: $PWD"
 
-# Arrayjob
-PYTHONPATH=$PWD python own_pipeline/HPO_random_search.py --seed $SLURM_ARRAY_TASK_ID
+eval "$($CONDA_BIN shell.bash hook)"
+conda activate $CONDA_WORKSPACE_NAME
+echo Activated conda environment
 
-# Done
+echo Running work...
+python -m own_pipeline.HPO_random_search --seed $MOAB_JOBARRAYINDEX
+
 echo "DONE"
 echo "Finished at $(date)"
