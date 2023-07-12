1ccfe80afc7d53d42c84b4bc46f4507d18861ef8
Alken0 <54180229+Alken0@users.noreply.github.com>
2023-07-12 16:28:42 +0200

    improve arguments of evaluate_ensembles

--- own_pipeline/evaluate_ensembles.py
+++ own_pipeline/evaluate_ensembles.py
@@ -21,77 +21,36 @@ def parse_arguments() -> Namespace:
         help="Index of GPU device to use. For CPU, set to -1. Default: -1.",
     )
     parser.add_argument(
-        "--arch_id",
-        type=int,
-        default=None,
-        help="Only used for DeepEns (RS) + ESA",
-    )
-    parser.add_argument(
-        "--esa",
+        "--baselearner_dir",
         type=str,
-        default="beam_search",
-        help="Ensemble selection algorithm. See nes/ensemble_selection/esas.py. Default: beam_search.",
+        default="saved_model"
     )
-    parser.add_argument("--M", type=int, default=5, help="Ensemble size. Default: 5.")
     parser.add_argument(
-        "--save_dir",
+        "--ensemble_dir",
         type=str,
-        help="Directory to save ensemble evaluation data for eventual plotting.",
+        default="saved_ensembles"
     )
     parser.add_argument(
-        "--load_bsls_dir",
+        "--ensemble_name",
         type=str,
-        help="Directory where the baselearners in the pool are saved. Will usually depend on --method.",
-    )
-    parser.add_argument(
-        "--load_ens_chosen_dir",
-        type=str,
-        help="Directory where output of ensembles_from_pools.py is saved. *Only used when --method is nes_rs or nes_re.*",
-    )
-    parser.add_argument(
-        "--incumbents_dir",
-        type=str,
-        help="Directory where output of rs_incumbents.py is saved.",
-    )
-    parser.add_argument(
-        "--nes_rs_bsls_dir",
-        type=str,
-        help="Directory where nes_rs baselearners are saved. *Only used when --method is deepens_rs.*",
-    )
-    parser.add_argument(
-        "--method",
-        choices=["nes_rs", "nes_re", "deepens_rs", "deepens_darts",
-                 "deepens_amoebanet_50k", "nes_rs_oneshot", "nes_re_50k",
-                 "deepens_darts_anchor", "deepens_darts_50k", "nes_rs_50k",
-                 "deepens_amoebanet", "deepens_gdas", "deepens_minimum",
-                 "deepens_pcdarts", "darts_esa", "amoebanet_esa", "nes_rs_esa",
-                 "darts_rs", "darts_hyper", "joint", "nes_rs_darts"],
-        type=str,
-    )
-    parser.add_argument(
-        "--dataset", choices=["cifar10", "cifar100", "fmnist", "imagenet", "tiny"], type=str, help="Dataset."
-    )
-
-    parser.add_argument(
-        "--validation_size",
-        type=int,
-        default=-1,
+        help="create an ensemble with 'create_ensemble' and pass the name of the .pt file here "
+             "- do not include the '.pt' ending",
+        required=True
     )
 
     return parser.parse_args()
 
 
 def load_baselearners(args: Namespace) -> List[Baselearner]:
-    id_set: set[int] = torch.load("saved_ensembles/ensemble_5_baselearners.pt")
+    id_set: set[int] = torch.load(f"{args.ensemble_dir}/{args.ensemble_name}.pt")
     POOL_NAME = "own_rs"
-    BASELEARNER_DIR = "saved_model"
 
     model_seed_list = [model_seeds(arch=seed, init=seed, scheme=POOL_NAME) for seed in id_set]
     baselearners = [
         load_baselearner(
             model_id=k,
             load_nn_module=False,
-            baselearner_dir=BASELEARNER_DIR
+            baselearner_dir=args.baselearner_dir
         )
         for k in model_seed_list
     ]
@@ -125,7 +84,7 @@ def main():
     baselearners = load_baselearners(args)
     ensemble = load_ensemble(args, baselearners)
     evaluate_ensemble(ensemble)
-    print(ensemble.evals)
+    print(dict(ensemble.avg_baselearner_evals))
 
 
 if __name__ == '__main__':
