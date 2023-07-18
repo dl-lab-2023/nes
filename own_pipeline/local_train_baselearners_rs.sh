0174e7a7043d82369af9c13dd79c4b1d8ca8038b
cnmicha <10116544+cnmicha@users.noreply.github.com>
2023-07-18 13:41:59 +0200

    Refactoring

--- own_pipeline/local_train_baselearners_rs.sh
+++ own_pipeline/local_train_baselearners_rs.sh
@@ -4,5 +4,5 @@ set -e
 max_seed=10
 
 for ((i=0; i<=max_seed; i++)); do
-   python -m own_pipeline.HPO_random_search --seed $i --openml_task_id=233088
+   python -m own_pipeline.train_baselearners_rs --seed $i --openml_task_id=233088
 done
