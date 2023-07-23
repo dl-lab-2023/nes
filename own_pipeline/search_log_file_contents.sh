88fed851d0b28293faf9906a39bedc0923dec026
cnmicha <10116544+cnmicha@users.noreply.github.com>
2023-07-23 02:50:36 +0200

    Add helper script to search log files

---
+++ own_pipeline/search_log_file_contents.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <directory_path> <search_string>"
+  exit 1
+fi
+
+directory_path="$1"
+search_string="$2"
+
+# Check if the specified directory exists
+if [ ! -d "$directory_path" ]; then
+  echo "Error: Directory not found: $directory_path"
+  exit 1
+fi
+
+# Loop through each file in the directory
+for file in "$directory_path"/*; do
+  # Check if the file is a regular file (not a directory or a special file)
+  if [ -f "$file" ]; then
+    # Use grep to search for the string in the file
+    if grep -i -q "$search_string" "$file"; then
+      echo "$file"
+    fi
+  fi
+done
