7bab517d95f805d3588d61f536951ee29c82d09b
Alken0 <54180229+Alken0@users.noreply.github.com>
2023-07-25 19:37:57 +0200

    change plots
    - change colors
    - adjust size
    - allow ranking to have equal accuracies
    - show ranking in %

--- own_pipeline/plotting/box_plot_accuracy.py
+++ own_pipeline/plotting/box_plot_accuracy.py
@@ -12,20 +12,23 @@ def box_plot_accuracy(args: Namespace, stats: dict):
             all_data.append(stats[method][task])
         data[method] = all_data
 
-    fig = plt.figure(dpi=args.dpi, figsize=(12.8, 9.6))
+    fig = plt.figure(dpi=args.dpi, figsize=(20, 10))
     plt.title("Accuracy of Ensembles")
     box_plot_data = [data[s] for s in data.keys()]
-    plt.boxplot(
+    bplot = plt.boxplot(
         box_plot_data,
         positions=range(2, (len(data.keys()) + 1) * 2, 2),
         patch_artist=True,
         showmeans=False,
         showfliers=False,
         medianprops={"color": "white", "linewidth": 1.5},
-        boxprops={"facecolor": "C0", "edgecolor": "white", "linewidth": 1.5},
-        whiskerprops={"color": "C0", "linewidth": 1.5},
-        capprops={"color": "C0", "linewidth": 1.5}
+        boxprops={"edgecolor": "white", "linewidth": 1.5},
+        whiskerprops={"linewidth": 1.5},
+        capprops={"linewidth": 1.5}
     )
+    colors = ['tab:orange', 'tab:red', 'tab:purple', 'tab:blue', 'tab:green', 'tab:olive']
+    for patch, color in zip(bplot['boxes'], colors):
+        patch.set_facecolor(color)
     plt.ylabel("percentage points")
     plt.xticks(range(2, (len(data.keys()) + 1) * 2, 2), labels=data.keys())
     plt.tight_layout()
